551ac676496eb0575d0a9286e001441da4d5bb5a
moshithakolanjiyappan <moshithakolanjiyappan@gmail.com>
2025-06-24 13:47:47 +0530

    Update smallest_elements.py

--- assignment-2/smallest_elements.py
+++ assignment-2/smallest_elements.py
@@ -1,3 +1,8 @@
-numbers = [12, 4, 56, 2, 89, 3]
-smallest = min(numbers)
-print("The smallest element is:", smallest)
+lst = [7, 2, 5, 3, 9, 1]
+k = 3
+sorted_lst = sorted(lst)
+if 1 <= k <= len(lst):
+    kth_smallest = sorted_lst[k-1]
+    print(kth_smallest)
+else:
+    print("k is out of range")
